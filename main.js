6ee5c1f71f20c6622c44bb3eb630231a7c5de6af
hajihye123 <hajihye123@gmail.com>
2022-01-07 00:35:23 +0900

    localstorage 추가, delete기능 추가

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 'use strict';
+const TODOS_KEY = "todos"
 
-let itemList = [];
-let input_button = document.querySelector(".input_button");
-input_button.addEventListener("click", addItem);
+let itemList = [];  // 할 일을 저장하는 리스트
+let input_btn = document.querySelector(".input_button");
+input_btn.addEventListener("click", addItem);    // 추가 버튼에 클릭 리스너를 연결
 
 // 리스트에 추가
 function addItem() {
@@ -12,7 +13,21 @@ function addItem() {
         document.querySelector(".item").value = "";
         document.querySelector(".item").focus();
     }
+    saveTodos();
     showList();
+}
+
+// 리스트에서 삭제
+function deleteItem() {
+    let id = this.getAttribute("id");
+    itemList.splice(id, 1);
+    saveTodos();
+    showList();
+}
+
+// localstorage에 저장하기 - addItem, deleteItem 진행 후 실행해서 localstorage 업데이트 필요함
+function saveTodos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(itemList));  // 배열처럼 보이는 문자열을 저장
 }
 
 // 리스트 보여주기
@@ -20,9 +35,26 @@ function showList() {
     let list = "<ul>";
     // 내용 추가
     for (let i = 0; i < itemList.length; i++) {
-        list += "<li>" + itemList[i] + "</li>";
+        list += "<li>" + itemList[i] + "<span class='close' id=" + i + ">" + "삭제" + "</span></li>";
     }
     list += "</ul>";
     document.querySelector(".item_list").innerHTML = list;
+
+    let delete_btn = document.querySelectorAll(".close");
+    for (let i = 0; i < delete_btn.length; i++) {
+        delete_btn[i].addEventListener("click", deleteItem);
+    }
     
 }
+
+// 초기화
+function init() {
+    const savedTodos = localStorage.getItem(TODOS_KEY);
+    if (savedTodos !== null) {
+        const parsedToDos = JSON.parse(savedTodos);     // 배열같은 문자열 "[a,b,c]"를 진짜 배열 [a,b,c]로 만들기
+        itemList = parsedToDos;     // localstorage에 저장된 내용을 기존 배열에 담아줌-> 새로고침할 때마다 갱신됨
+    }
+    showList();
+}
+
+init();
